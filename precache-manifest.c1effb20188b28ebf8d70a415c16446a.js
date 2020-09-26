1d4c412a5a6e947aa989312ee8291094e53d1b6d
adarsh <adasrhthenoob@gmail.com>
2020-09-27 00:42:38 +0530

    Updates

---
+++ precache-manifest.c1effb20188b28ebf8d70a415c16446a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37f053473703d4c3d96eef87e19c6fe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f84429c47c0066ed5b24",
+    "url": "/monsters-rolodex/static/css/main.98852052.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/monsters-rolodex/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84429c47c0066ed5b24",
+    "url": "/monsters-rolodex/static/js/main.c138a337.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
